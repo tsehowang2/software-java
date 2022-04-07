49065f73e304ddb36e7e47204c9f2b3a2b1ed122
Alan <alanhltong@gmail.com>
2022-04-07 18:46:51 +0800

    changed test

--- src/test/java/comp3111/covid/UnitTest.java
+++ src/test/java/comp3111/covid/UnitTest.java
@@ -1,32 +1,19 @@
 package comp3111.covid;
-
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 public class UnitTest {
-
-	//static Case a = new Case();
-	public static void main(String arg[]) {
-		System.out.println("Hello, UnitTest!");	
-	}
-	
-	public static int computeOne() {
-		return 1;
-	}
-	
-	public static boolean isItTrue() {
-		return true;
-	}
-	
-	public static boolean isEvenNumber(int num) {
-		if ((num % 2) == 0)
-			return true;
-		else
-			return false;
-	}
-	
-	public static int[] sort(int[] anyArray) {
-		return new int[] {1, 2, 3, 4, 5};
-	}
-	
-	/*public static String getIsoCode() {
-		return a.getIsoCode();
-	}*/
+  int age;
+  @Before
+  public void setUp() throws Exception {
+    age = 30;
+  }
+  @Test
+  public void getIsoCodeWithValidInput() {
+    assertEquals("CHN", Case.getIsoCode());
+  }
+  @Test
+  public void getMedianAgeWithValidInput() {
+    assertEquals(age, Country.getMedianAge());
+  }
 }
