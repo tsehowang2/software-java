c94b9379d033e608043f51737ab47a924f108b62
Leung Chun Hei <chleungba@connect.ust.hk>
2022-04-07 14:27:17 +0800

    class country and case

--- src/main/java/comp3111/covid/Case.java
+++ src/main/java/comp3111/covid/Case.java
@@ -2,4 +2,29 @@ package comp3111.covid;
 
 public class Case {
 	String iso_code;
+	int new_cases;
+	int new_cases_per_million;
+	int new_cases_smoothed;
+	int new_cases_smoothed_per_million;
+	int new_deaths;
+	int new_deaths_per_million;
+	int new_deaths_smoothed;
+	int new_deaths_smoothed_per_million;
+	int tests_per_case;
+	int new_tests;
+	int new_tests_per_thousand;
+	int new_tests_smoothed;
+	int new_tests_smoothed_per_thousand;
+	int new_vaccinations;
+	int new_vaccinations_smoothed;
+	int new_vaccinations_smoothed_per_million;
+	int icu_patients;
+	int icu_patients_per_million;
+	int weekly_icu_admissions;
+	int weekly_icu_admissions_per_million;
+	int hosp_patients;
+	int hosp_patients_per_million;
+	int weekly_hosp_admissions;
+	int weekly_hosp_admissions_per_million;
+	int hospital_beds_per_thousand;
 }
--- src/main/java/comp3111/covid/Country.java
+++ src/main/java/comp3111/covid/Country.java
@@ -2,5 +2,37 @@ package comp3111.covid;
 
 public class Country {
 	String iso_code;
-
+	String continent;
+	String location;
+	float reproduction_rate;
+	float tests_units;
+	float population_density;
+	int median_age;
+	int aged_65_older;
+	int aged_70_older;
+	int gdp_per_capita;
+	int extreme_poverty;
+	float cardiovasc_death_rate;
+	int diabetes_prevalence;
+	int female_smokers;
+	int male_smokers;
+	float life_expectancy;
+	int handwashing_facilities;
+	int human_development_index;
+	int excess_mortality;
+	int population;
+	int stringency_index;
+	float positive_rate;
+	int people_fully_vaccinated;
+	int people_fully_vaccinated_per_hundred;
+	int people_vaccinated;
+	int people_vaccinated_per_hundred;
+	int total_vaccinations;
+	int total_vaccinations_per_hundred;
+	int total_tests;
+	int total_tests_per_thousand;
+	int total_cases;
+	int total_cases_per_million;
+	int total_deaths;
+	int total_deaths_per_million;
 }
